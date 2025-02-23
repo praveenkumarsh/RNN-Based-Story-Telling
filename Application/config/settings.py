5c52024aac6e82bd740ee95cd39f203132f19e9c
Praveen Sharma <dce.pks@gmail.com>
2025-02-24 00:37:31 +0530

    Updated changes

--- Application/config/settings.py
+++ Application/config/settings.py
@@ -2,8 +2,8 @@ import torch
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL_PARAMS = {
-    "embed_size": 256,
-    "hidden_size": 512,
+    "embed_size": 1024,
+    "hidden_size": 2048,
     "vocab_threshold": 5,
 }
 # Add constants
